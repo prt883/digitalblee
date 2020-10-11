a6cce4d27391ccdc212f75ee8570711d4df009a2
praveen <pk.251094@gmail.com>
2020-10-11 23:46:24 +0530

    initial commit

---
+++ src/main/java/com/marketing/digitalblee/DigitalbleeApplication.java
@@ -0,0 +1,13 @@
+package com.marketing.digitalblee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalbleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalbleeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketig/digitalblee/DigitalbleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketig.digitalblee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalbleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
